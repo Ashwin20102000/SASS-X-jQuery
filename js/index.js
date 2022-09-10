4549b21b0b9730c784b415addbaece6f1db873e6
Ashwin.J <Ashwin.J@codingmart.com>
2022-09-10 22:07:59 +0530

    intial commit

---
+++ js/index.js
@@ -0,0 +1,74 @@
+function iconsIntializer(){
+  $('.handleIcon').css({
+    cursor:'pointer',
+  })
+}
+
+function bringCardsToShow(){
+  setTimeout(()=>{
+    $('.about_me').fadeIn('slow').css('display','flex')
+    $('.handleIcon').fadeIn('slow')
+  },
+  1000)
+}
+
+function scaleWhenHover(selectors){
+  $(selectors).each(function(idx,selector){
+    $(selector).hover(function(){
+      $(this).css({"transform":"scale(1.1)","transition":'1s'})
+    },function(){
+      $(this).css({"transform":"scale(1)","transition":'1s'})
+    }) 
+  })
+
+}
+
+function scrollVisible(){
+  $('#scroll').addClass('occupier');
+  $('.projects').hide()
+  $(document).scroll(function(){
+    if($(this).scrollTop()>500){
+      $('#scroll').removeClass('occupier');
+      $('.projects').fadeOut(2000)
+    }
+  })
+}
+const loadOtherProjects = () => {
+  const otherProjects = [
+    {
+      title:'AlgoX',
+      description:'Algorithm Vishuvalizer',
+      img:'../assests/AlgoX.png'
+    }
+  ];
+  $.each(otherProjects,function(idx,project){
+    $('#otherProjects').append(
+      `
+      <div class="otherProjects">
+        <h1>${project.title}</h1>
+        <span class="my-2">${project.description}</span>
+        <img class="project_img"  src=${project.img}>
+      </div>
+      `
+    )
+  })
+}
+
+
+const intilizeFunctions = () => {
+  const scaleHoverSelectors = ['.contain-image','.content']
+  scrollVisible()
+  scaleWhenHover(scaleHoverSelectors)
+  iconsIntializer();
+  bringCardsToShow();
+  loadOtherProjects();
+}
+
+$(document).ready(function () {
+  intilizeFunctions();
+  (function loop() {
+    $('.fadeTex').delay(900).fadeToggle("slow",loop);
+  }())
+
+});
+
